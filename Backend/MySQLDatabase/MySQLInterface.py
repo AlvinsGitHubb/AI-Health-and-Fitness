bdd8b9ecbd788921ceb737936ac91606c39fd7d8
Save-1401 <save1401m@gmail.com>
2024-10-10 15:06:45 -0500

    Added MYSQL interface functions
    For account creation, logging in, getting user data, logging workouts, logging meals.

--- Backend/MySQLDatabase/MySQLInterface.py
+++ Backend/MySQLDatabase/MySQLInterface.py
@@ -1,12 +1,13 @@
+from ast import Attribute
 import mysql.connector
 
 class MySQLInterface(object):
 
     def __init__(self, _host, _user, _password, _database):
-        self.host = _host
-        self.user = _user
-        self.password = _password
-        self.database = _database
+        self.host = _host #The server’s IP address or domain name
+        self.user = _user #The host's username
+        self.password = _password #The host's password
+        self.database = _database # The name of the database being accessed
 
     #Connects to the database named _database
     def ConnectToDatabase(self):
@@ -16,63 +17,71 @@ class MySQLInterface(object):
             password=self.password,
             database=self.database #In createDatabase.py the created database is named AIHealthAndFitnessDatabase
         )
+        self.mycursor = self.mydb.cursor()
 
     #Adds an item to mydb of type tableName
-    def AddItem(self, tableName, rows, vals):
-        mycursor = self.mydb.cursor()
-        sql = "INSERT INTO " + tableName + " "
-        rowsString = "(" + rows[0]
+    def AddItem(self, tableName, columns, vals):
+        sql = f"INSERT INTO {tableName} "
+        rowsString = "(" + columns[0]
         valuesString = "(%s"
-        for x in range(len(rows) - 1):
+        for x in range(len(columns) - 1):
             x = x + 1
-            rowsString += ", " + rows[x]
+            rowsString += ", " + columns[x]
             valuesString += ", %s"
         rowsString += ")"
         valuesString += ")"
         sql += rowsString + " VALUES " + valuesString
-        print(sql)
-        mycursor.execute(sql, vals)
+        self.mycursor.execute(sql, vals)
         self.mydb.commit()
         
     #Sets an attribute "attribute" of an item in mydb of type tablename
-    def SetItemAttribute(self, tableName, attribute, toSetTo, _id):
-        mycursor = self.mydb.cursor()
-        sql = "UPDATE " + tableName + " SET " + attribute + " = %s WHERE id = %s"
-        val = (toSetTo, _id)
-        mycursor.execute(sql, val)
+    def SetItemAttribute(self, tableName, attribute, toSetTo, id_):
+        sql = f"UPDATE {tableName} SET {attribute} = %s WHERE id = %s"
+        val = (toSetTo, id_)
+        self.mycursor.execute(sql, val)
         self.mydb.commit()
         
     #Gets all of the items in a table
     def GetTable(self, tableName):
-        mycursor = self.mydb.cursor()
-        sql = "SELECT * FROM " + tableName;
-        mycursor.execute(sql)
-        results = mycursor.fetchall()
+        sql = f"SELECT * FROM {tableName}"
+        self.mycursor.execute(sql)
+        results = self.mycursor.fetchall()
         return results
 
     #Gets all of the data from one item of type tablename
-    def GetItem(self, tableName, _id):
-        mycursor = self.mydb.cursor()
-        sql = "SELECT * FROM " + tableName + " WHERE id = %s"
-        mycursor.execute(sql, _id)
-        results = mycursor.fetchone()
+    def GetItem(self, tableName, id_):
+        sql = f"SELECT * FROM {tableName} WHERE id = {id_}"
+        self.mycursor.execute(sql)
+        results = self.mycursor.fetchone()
+        return results
+
+    #Gets all of the data from one item of type tablename
+    def GetItemBasedOnAttribute(self, tableName, attribute, value):
+        sql = f"SELECT * FROM {tableName} WHERE {attribute} = {value}"
+        self.mycursor.execute(sql)
+        results = self.mycursor.fetchone()
+        return results
+    
+    #Gets all of the data from one item of type tablename
+    def GetItemsBasedOnAttribute(self, tableName, attribute, value):
+        sql = f"SELECT * FROM {tableName} WHERE {attribute} = {value}"
+        self.mycursor.execute(sql)
+        results = self.mycursor.fetchall()
         return results
 
     #Gets an attribute "attribute" from an item in mydb of type tablename
-    def GetItemAttribute(self, tableName, attribute, _id):
-        mycursor = self.mydb.cursor()
-        sql = "SELECT " + attribute + " FROM "+ tableName + " WHERE id = %s"
-        mycursor.execute(sql, _id)
-        myresult = mycursor.fetchone()
+    def GetItemAttribute(self, tableName, attribute, id_):
+        sql = f"SELECT {attribute} FROM {tableName} WHERE id = {id_}"
+        self.mycursor.execute(sql)
+        myresult = self.mycursor.fetchone()
         return myresult
+
+    def GetLastInsertId(self):
+       self.mycursor.execute("SELECT LAST_INSERT_ID()")
+       return self.mycursor.fetchone()[0]  # Fetches the ID from the result
 
     #I put this here just for testing code and editing the database values easily it should not be used in the final code project
     def execute(self, command):
-        mycursor = self.mydb.cursor()
-        mycursor.execute(command)
+        self.mycursor.execute(command)
 
     pass 
-
-
-
-
--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,76 @@
+from asyncio.windows_events import NULL
+from MySQLDatabase import user, MySQLInterface, workoutManager, mealManager
+from datetime import datetime, timedelta
 
+sqlInterface = MySQLInterface.MySQLInterface("localhost", "root", "*tr5d7Ka9q", "AIHealthAndFitnessDatabase2");
+sqlInterface.ConnectToDatabase()
+
+#ALL OF THIS CODE THAT IS CURRENTLY IN MAIN IS JUST FOR TESTING AND MAKING SURE THAT ALL OF THE FUNCTIONS I MADE WORK IT WILL NOT BE PART OF THE FINAL PROJECT
+userId = -1
+while userId == -1:
+    option = input("Create Account (0), Login (1), or quit (2): ")
+    if option == "0":
+        email = input("Please enter an email: ")
+        password = input("Please input a password: ")
+
+        userId = user.CreateAccount(sqlInterface, email, password)
+        print(f"Account successfully created and loggd in. Your user ID is: {userId}")
+    elif option == "1":
+        email  = input("Enter your email: ")
+        password = input("Enter your password: ")
+        
+        userId = user.Login(sqlInterface, email, password)
+        if userId >= 0:
+            print(f"You have successfully logged in. Your user ID is: {userId}")
+        else:
+            print("Invalid username or password!")
+    elif option == "2":
+        break
+    else:
+        print("Invalid Option")
+
+if userId != -1:
+    option = input("What would you like to do? View User Data(0), Update User Data(1), Log Workout(2), View Workouts(3), Log Meal(4), View Meals(5), Speak With Chatbot(6), Get Exercice Recommendation(7), Get Meal Recommendation(8), Logout (9): ")
+    if option == "0":
+        _user = user.GetUserAttributes(sqlInterface, userId)
+        for x in _user:
+            print(x)
+    elif option == "1":
+        toChange = input("What value would you like to update? name, sex, age, height, weight, diabetes, fitnessGoal: ")
+        toChangeTo = input("What would you like to change that value to?: ")
+        user.SetUserAttribute(sqlInterface, toChange, toChangeTo, userId)
+    elif option == "2":
+        workoutType = input("What type of workout did you do?: ")
+        duration = input("How long was your workout (in minutes)?: ")
+        date = input("What day did you do your workout? (YYYY-MM-DD): ")
+        sets = []
+        while True:
+            print("Adding a set.")
+            exerciseId = input("Enter an exerciseId: ")
+            reps = input("How many reps did you do?: ")
+            weight = input ("How much weight did you use?: ")
+            sets.append((exerciseId, reps, weight))
+            cont = input("Do you want to add another set? No(0), Yes(1): ")
+            if cont == "0":
+                break
+
+        workoutManager.LogWorkout(sqlInterface, userId, workoutType, 0, duration, date, sets)
+    elif option == "3":
+        _workouts = workoutManager.GetWorkouts(sqlInterface, userId)
+        for x in _workouts:
+            print(x[0])
+            if x[1] != NULL:
+                for y in x[1]:
+                    print(y)
+    elif option == "4":
+        mealType = input("What type of meal did you have?: ")
+        calories = input("How many calories were in the meal?: ")
+        protein = input("How much protein was in the meal?: ")
+        carbs = input("How much carbs was in the meal?: ")
+        fat = input("How much fat was in the meal?: ")
+        date = input("What day did you eat the meal? (YYYY-MM-DD): ")
+        mealManager.LogMeal(sqlInterface, userId, mealType, calories, protein, carbs, fat, date)
+    elif option == "5":
+        _meals = mealManager.GetMeals(sqlInterface, userId)
+        for x in _meals:
+            print(x)
