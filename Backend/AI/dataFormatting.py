9aa6fced1ea8d2ae23ca1a474d9a057a842d5e2a
Save-1401 <75083914+Save-1401@users.noreply.github.com>
2024-10-11 13:54:00 -0500

    I need to commit these in order to revert this is not a commit

--- Backend/AI/dataFormatting.py
+++ Backend/AI/dataFormatting.py
@@ -4,12 +4,17 @@ import json
 with open('gymrecommendations.json', 'r') as file:
     data = json.load(file)
 
+#types_ = []
+itemName = "Exercises"
 for item in data:  # Assuming 'data' is a list of dictionaries
-    if item['Diabetes'] == "No":
-        item['Diabetes'] = False
+    #if item[itemName] not in types_:
+        #types_.append(item[itemName])
+        #print(item[itemName])
+    if item[itemName] == 'Squats, deadlifts, bench presses, and overhead presses':
+        item[itemName] = 0
     else:
-        item['Diabetes'] = True
+        item[itemName] = 1
 
 #Step 3: Save the modified data back to the file
 with open('gymrecommendations.json', 'w') as file:
-    json.dump(data, file, indent=4)  # Nice formatting with that indent, bro
+    json.dump(data, file, indent=4)
