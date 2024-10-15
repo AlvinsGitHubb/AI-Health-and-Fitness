9b7984a7346a7335adbef62130ef0cf3f867a2ae
Save-1401 <save1401m@gmail.com>
2024-10-15 14:52:43 -0500

    Trained a model
    Trained and saved a model to predict the fitness type the user should perform.

--- Backend/AI/trainingAI.py
+++ Backend/AI/trainingAI.py
@@ -3,6 +3,8 @@ from tensorflow.keras import Model, Input
 from tensorflow.keras.layers import Dense
 import numpy as np
 import json
+import datetime
+import os
 
 input_columns = ['Sex_Male', 'Sex_Female', 'Age', 'Height', 'Weight', 'Diabetes', 'Fitness Goal_Weight Loss', 'Fitness Goal_Weight Gain']
 output_columns = ['Fitness Type_Muscular Fitness', 'Fitness Type_Cardio Fitness']
@@ -19,19 +21,36 @@ inputs = []
 outputs = []
 
 for entry in data:
+    input_row = []
     for col in input_columns:
         try:
-            input_row = entry[col]
-            inputs.append(input_row)
+            input_column = entry[col]
+            input_row.append(input_column)
         except:
-            inputs.append(False)
-            
+            input_row.append(False)
+    inputs.append(input_row)
+    
+    output_row = []
     for col in output_columns:
         try:
-            output_row = entry[col]
-            inputs.append(output_row)
+            output_column = entry[col]
+            output_row.append(output_column)
         except:
-            inputs.append(False)
+            output_row.append(False)
+    outputs.append(output_row)
+
+#print("-----INPUTS 1------")
+#print(inputs)
+#print("-----OUTPUTS 1------")
+#print(outputs)
+
+inputs = np.array(inputs, dtype=np.float32)
+outputs = np.array(outputs, dtype=np.float32) 
+
+#print("-----INPUTS 2------")
+#print(inputs)
+#print("-----OUTPUTS 2------")
+#print(outputs)
 
 if inputs.size == 0 or outputs.size == 0:
     raise ValueError("Inputs or Outputs array is empty. Check your data source.")
@@ -42,27 +61,34 @@ if len(inputs.shape) == 1:
 print(f"Shape of inputs: {inputs.shape}")
 
 input_1 = Input(shape=(inputs.shape[1],))
-x = Dense(64, activation='rulu')(input_1)
-x = Dense(32, activation='rulu')(x)
-x = Dense(16, activation='rulu')(x)
-x = Dense(8, activation='rulu')(x)
+x = Dense(64, activation='relu')(input_1)
+x = Dense(32, activation='relu')(x)
+x = Dense(16, activation='relu')(x)
+x = Dense(8, activation='relu')(x)
 
 output = Dense(outputs.shape[1], activation='softmax')(x)
 
+model = Model(inputs=input_1, outputs=output)
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-history= model.fit(inputs, outputs, epochs=100, batch_size=32, validation_split=0.2)
+history= model.fit(inputs, outputs, epochs=25, batch_size=32, validation_split=0.2)
 
-new_data = np.array([[2, 2, 2, 4],
-                     [3, 2, 0, 5],
-                     [7, 8, 4, 0],
-                     [1, 3, 4, 7]])
+#new_data = np.array([[2, 2, 2, 4],
+ #                    [3, 2, 0, 5],
+  #                   [7, 8, 4, 0],
+   #                  [1, 3, 4, 7]])
 
-predictions = model.predict(new_data)
-print(f"Predicted Output: {predictions}")
+#predictions = model.predict(new_data)
 
-model.save("file/path")
+#print(f"Predicted Output: {predictions}")
 
-loaded_model = tf.keras.models.load_model("file/path")
-predictions = loaded_model.predict(new_data)
+timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+model_save_path = f"W:\\GitHub\\Ai Health and Fitness\\AI-Health-and-Fitness\\Backend\\AI\\fitness_model_{timestamp}"
+print(model_save_path)
+
+model.export(str(model_save_path))
+
+#loaded_model = tf.keras.models.load_model(f"{model_save_path}//saved_model.pb")
+predictions = model.predict(inputs)
 print(f"Predicted Output: {predictions}")
