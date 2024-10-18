ceb4bf154cc222a2c802225ab94e2b6077db8afb
AlvinsGitHubb <alvinmathew0627@gmail.com>
2024-10-17 21:49:34 -0500

    Took off one problem

--- Frontend/test/widget_test.dart
+++ Frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:fitness/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:fitness_workout_app_flutter_3_ui/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
